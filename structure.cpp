f8ac8bbf4c1d14f273655f6d1f345ded379ec97e
Harsh-sd <harshsandilya05042002@gmail.com>
2024-03-12 19:33:45 +0530

    printed id and char of employee

--- structure.cpp
+++ structure.cpp
@@ -15,5 +15,7 @@ int main()
     harry.favchar = 'C';
 
     cout << "the salary of employee is" << harry.salary << endl;
+    cout << "the id of employee is" << harry.id << endl;
+    cout << "the favchar of employee is" << harry.favchar << endl;
     return 0;
 }
